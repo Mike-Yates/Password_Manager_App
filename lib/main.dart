35a4f38363927e69ce39354e39e3d730324e47ce
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-01-20 21:00:58 -0500

    initialize the repo

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'password_calculator.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Retrieve Text Input',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+  final myController2 = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    myController2.dispose();
+    super.dispose();
+  }
+
+  //String calculatePassword(String keyword, String pin){
+  //  return keyword + pin;
+  //}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Yates/Logan - Password Manager'),
+      ),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/squad.jpg'),
+                fit: BoxFit.cover,
+              )
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              Text('Type Pin (4 digits)',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blue,
+                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  color: Colors.white.withOpacity(0.5),
+                  child: TextField(
+                    controller: myController,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50.0),
+              Text('Type Key (Case Sensitive)',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  color: Colors.white.withOpacity(0.5),
+                  child: TextField(
+                    controller: myController2,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0),
+              //Expanded(
+                //  flex: 1,
+                 // child: Image.asset('assets/squad.jpg')
+              //),
+              //SizedBox(height: 50.0),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // When the user presses the button, show an alert dialog containing
+        // the text that the user has entered into the text field.
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                // call on password calculator function
+                content: Text(calculatePassword(myController.text, myController2.text)),
+                actions: [
+                  TextButton(onPressed: (){
+                    Clipboard.setData(ClipboardData(text: calculatePassword(myController.text, myController2.text)));
+                  }, child: Text('Copy to clipboard')),
+                ],
+              );
+            },
+          );
+        },
+        tooltip: 'Show me the value!',
+        child: const Icon(Icons.done),
+      ),
+    );
+  }
+}
